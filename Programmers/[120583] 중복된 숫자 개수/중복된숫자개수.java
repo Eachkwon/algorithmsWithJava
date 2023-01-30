a7e4b8651db62ab925d25560daf7f4782b57d9ea
eachkwon@naver.com <eachkwon@naver.com>
2023-01-30 20:11:39 +0900

    Upload: 01.30 / [중복된 숫자 개수]

---
+++ Programmers/[120583] 중복된 숫자 개수/중복된숫자개수.java
@@ -0,0 +1,11 @@
+package Programmers;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        int result = 0;
+        for(int num: array) {
+            if(num==n) { result++; }
+        }
+        return result;
+    }
+}
