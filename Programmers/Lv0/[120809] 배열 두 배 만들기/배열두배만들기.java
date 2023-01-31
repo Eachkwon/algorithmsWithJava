5c3abf2111c60e75678d0c87fac0ae56efa738eb
eachkwon@naver.com <eachkwon@naver.com>
2023-01-31 17:37:43 +0900

    Upload: 01.31 / [배열 두 배 만들기]

---
+++ Programmers/Lv0/[120809] 배열 두 배 만들기/배열두배만들기.java
@@ -0,0 +1,10 @@
+package Programmers.Lv0;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        for(int i=0; i<numbers.length; i++) {
+            numbers[i] *=2;
+        }
+        return numbers;
+    }
+}
