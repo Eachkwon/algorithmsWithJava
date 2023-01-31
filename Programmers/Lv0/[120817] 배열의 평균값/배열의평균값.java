78c8d32ff1147fb4ef887a61c9f7f74c7ce852b3
eachkwon@naver.com <eachkwon@naver.com>
2023-01-31 17:38:57 +0900

    Upload: 01.31 / [배열의 평균값]

---
+++ Programmers/Lv0/[120817] 배열의 평균값/배열의평균값.java
@@ -0,0 +1,12 @@
+package Programmers.Lv0;
+
+class Solution {
+    public double solution(int[] numbers) {
+        int sum=0;
+        for(int i=0; i<numbers.length; i++) {
+            sum+= numbers[i];
+        }
+
+        return (double)sum/numbers.length;
+    }
+}
