756b3ebdb2a64273642d647e6419e7247d48ecdc
eachkwon@naver.com <eachkwon@naver.com>
2023-02-01 10:22:36 +0900

    Upload: 02.01 / [평균 구하기]

---
+++ Programmers/Lv1/[12944] 평균 구하기/평균구하기.java
@@ -0,0 +1,12 @@
+package Programmers.Lv1;
+
+class Solution {
+    public double solution(int[] arr) {
+        int sum = 0;
+        for(int num : arr) {
+            sum += num;
+        }
+
+        return (double)sum/arr.length;
+    }
+}
