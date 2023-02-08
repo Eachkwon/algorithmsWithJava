44c4bddbf8a0d297dabb84dddc10a4a2e0f9d166
eachkwon@naver.com <eachkwon@naver.com>
2023-02-08 16:25:42 +0900

    Upload: 02.08 / [예산]

---
+++ Programmers/Lv1/[12982] 예산/예산.java
@@ -0,0 +1,20 @@
+package Programmers.Lv1;
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int answer = 0;
+
+        for(int i=0; i<d.length; i++) {
+            if(d[i]<=budget) {
+                budget -= d[i];
+                answer++;
+            } else {
+                break;
+            }
+        }
+        return answer;
+    }
+}
