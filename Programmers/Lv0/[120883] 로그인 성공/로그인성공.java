942a7ef3b6722481b8c0664062e04b0e5ae6b290
eachkwon@naver.com <eachkwon@naver.com>
2023-02-09 22:35:00 +0900

    Upload: 02.09 / [로그인 성공]

---
+++ Programmers/Lv0/[120883] 로그인 성공/로그인성공.java
@@ -0,0 +1,18 @@
+package Programmers.Lv0;
+
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "fail";
+        for(int i=0; i<db.length; i++){
+            if(id_pw[0].equals(db[i][0])&&id_pw[1].equals(db[i][1])){
+                answer = "login";
+                break;
+            } else if(id_pw[0].equals(db[i][0])&&!id_pw[1].equals(db[i][1])){
+                answer = "wrong pw";
+                break;
+            }
+
+        }
+        return answer;
+    }
+}
