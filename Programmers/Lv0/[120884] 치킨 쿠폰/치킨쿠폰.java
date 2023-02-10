3832844d8ca5bf2a233008973c074d3b89e3f934
eachkwon@naver.com <eachkwon@naver.com>
2023-02-10 21:47:13 +0900

    Upload: 02.10 / [치킨 쿠폰]

---
+++ Programmers/Lv0/[120884] 치킨 쿠폰/치킨쿠폰.java
@@ -0,0 +1,14 @@
+package Programmers.Lv0;
+
+class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+
+        while(chicken>=10) {
+            answer += chicken/10;
+            chicken = chicken/10 + chicken%10;
+        }
+
+        return answer;
+    }
+}
