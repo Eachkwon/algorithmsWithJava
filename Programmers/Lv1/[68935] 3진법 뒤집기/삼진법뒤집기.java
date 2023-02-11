a59cb41dd65133475e9f438bd39f3d9155c20995
eachkwon@naver.com <eachkwon@naver.com>
2023-02-12 04:39:01 +0900

    Upload: 02.12 / [3진법 뒤집기]

---
+++ Programmers/Lv1/[68935] 3진법 뒤집기/삼진법뒤집기.java
@@ -0,0 +1,8 @@
+package Programmers.Lv1;
+
+class Solution {
+    public int solution(int n) {
+        String str = new StringBuilder(Integer.toString(n,3)).reverse().toString();
+        return Integer.parseInt(str,3);
+    }
+}
