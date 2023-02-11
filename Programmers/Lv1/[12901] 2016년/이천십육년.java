be78e50ec6aaf80223828473a90243b07de4f1ff
eachkwon@naver.com <eachkwon@naver.com>
2023-02-11 19:54:29 +0900

    Upload: 02.11 / [2016년]

---
+++ Programmers/Lv1/[12901] 2016년/이천십육년.java
@@ -0,0 +1,9 @@
+package Programmers.Lv1;
+
+import java.time.*;
+
+class Solution {
+    public String solution(int a, int b) {
+        return LocalDate.of(2016,a,b).getDayOfWeek().toString().substring(0,3);
+    }
+}
