02ad6e494d906f9785a8966fc9cf824e5e0009a2
eachkwon@naver.com <eachkwon@naver.com>
2023-02-13 22:44:40 +0900

    Upload: 02.13 / [숫자 문자열과 영단어]

---
+++ Programmers/Lv1/[81301] 숫자 문자열과 영단어/숫자문자열과영단어.java
@@ -0,0 +1,14 @@
+package Programmers.Lv1;
+
+class Solution {
+    public int solution(String s) {
+        String[] num = {"zero", "one", "two", "three", "four", "five", "six",
+                "seven", "eight", "nine"};
+
+        for(int i=0; i<=9; i++){
+            s = s.replace(num[i],Integer.toString(i));
+        }
+
+        return Integer.parseInt(s);
+    }
+}
