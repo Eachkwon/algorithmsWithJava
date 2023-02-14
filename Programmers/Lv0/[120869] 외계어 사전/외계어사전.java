a71125391eec9f8af8f15414ff2f1fafc5e59e68
eachkwon@naver.com <eachkwon@naver.com>
2023-02-14 10:07:07 +0900

    Upload: 02.14 / [외계어 사전]

---
+++ Programmers/Lv0/[120869] 외계어 사전/외계어사전.java
@@ -0,0 +1,24 @@
+package Programmers.Lv0;
+
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2;
+
+        for(int i=0; i<dic.length; i++) {
+            if(dic[i].length()==spell.length) {
+                int count = 0;
+                for(int j=0; j<spell.length; j++) {
+                    if(dic[i].contains(spell[j])) {
+                        count++;
+                    }
+
+                    if(count==spell.length) {
+                        return 1;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
