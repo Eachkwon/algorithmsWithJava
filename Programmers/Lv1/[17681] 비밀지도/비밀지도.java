a11b5d665c21f9168216d04fd042f92b14b3ffeb
eachkwon@naver.com <eachkwon@naver.com>
2023-02-19 15:29:58 +0900

    Revise: 02.19 / [비밀지도]

--- Programmers/Lv1/[17681] 비밀지도/비밀지도.java
+++ Programmers/Lv1/[17681] 비밀지도/비밀지도.java
@@ -1,33 +1,24 @@
-package Programmers.Lv2;
+package Programmers.Lv1;
 
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        String a, b, z;
+        String a;
 
         for(int i=0; i<n; i++){
-            a = Integer.toBinaryString(arr1[i]);
-            b = Integer.toBinaryString(arr2[i]);
+            a = Integer.toBinaryString(arr1[i] | arr2[i]);
 
             if(a.length()!=n) {
-                z = "";
+                StringBuilder z = new StringBuilder();
                 for(int j=0; j<n-a.length(); j++){
-                    z += "0";
+                    z.append("0");
                 }
-                a = z + a;
-            }
-
-            if(b.length()!=n) {
-                z = "";
-                for(int j=0; j<n-b.length(); j++){
-                    z += "0";
-                }
-                b = z + b;
+                a = z.toString() + a;
             }
 
             StringBuilder sb = new StringBuilder();
             for(int k=0; k<n; k++) {
-                if(a.charAt(k)=='0'&&b.charAt(k)=='0'){
+                if(a.charAt(k)=='0'){
                     sb.append(" ");
                 } else {
                     sb.append("#");
@@ -40,7 +31,7 @@ class Solution {
 }
 
 /*
-2.18ms
-2.22ms
-3.88ms 
+2.18ms → 0.07ms
+2.22ms → 1.36ms
+3.88ms → 1.19ms
  */
