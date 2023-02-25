da001c9711b83d7615e782f7728c1db65502d62e
eachkwon@naver.com <eachkwon@naver.com>
2023-02-26 07:44:12 +0900

    Revise: 02.26 / [예상 대진표]

--- Programmers/Lv2/[12985] 예상 대진표/예상대진표.java
+++ Programmers/Lv2/[12985] 예상 대진표/예상대진표.java
@@ -2,17 +2,20 @@ package Programmers.Lv2;
 
 class Solution {
     public int solution(int n, int a, int b) {
-        int answer = 1;
+        int answer = 0;
 
-        while(a+b!=3) {
-            if(a%2!=0) a++;
-            if(b%2!=0) b++;
-
-            a /= 2;
-            b /= 2;
+        while(a!=b) {
+            a = a/2 + a%2;
+            b = b/2 + b%2;
             answer++;
         }
 
         return answer;
     }
 }
+
+/*
+a, b가 홀수일 때 if문을 통해 1을 더해줬더니 테스트 돌릴 때 시간초과가 났다.
+
+if문을 빼고 2로 나눈 나머지를 더해줌으로써 해결하였다.
+ */
