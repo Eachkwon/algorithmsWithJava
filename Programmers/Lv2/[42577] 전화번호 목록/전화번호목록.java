0069fa173dc49b80954c4c7214d2c106b9e7b6e8
eachkwon@naver.com <eachkwon@naver.com>
2023-02-28 14:09:13 +0900

    Revise: 02.28 / [전화번호 목록]

--- Programmers/Lv2/[42577] 전화번호 목록/전화번호목록.java
+++ Programmers/Lv2/[42577] 전화번호 목록/전화번호목록.java
@@ -2,23 +2,23 @@ package Programmers.Lv2;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Arrays;
 
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
+        Arrays.sort(phone_book);
 
         HashMap<String,Integer> map = new HashMap<>();
         for(int i=0; i<phone_book.length; i++) {
-            map.put(phone_book[i],0);
+            map.put(phone_book[i],i);
         }
 
         for(Map.Entry<String,Integer> entry: map.entrySet()) {
             for(int i=0; i<phone_book.length; i++) {
-                if(phone_book[i].startsWith(entry.getKey()))
-                    map.put(entry.getKey(),entry.getValue()+1);
+                if(entry.getValue()!=i&&phone_book[i].startsWith(entry.getKey()))
+                    return false;
             }
-
-            if(entry.getValue()>=2) return false;
         }
 
         return answer;
@@ -26,7 +26,10 @@ class Solution {
 }
 
 /*
-97.71ms
-10.54ms
-115.57ms
+97.71ms → 61.70ms
+10.54ms → 18.42ms
+115.57ms → 92.40ms
+
+1. 같은 접두어를 가질 경우 value 값에 1씩 더해주고, for문을 돌린 뒤 value가 2 이상일 때 false를 return하였다.
+2. value에 idx를 넣고, 접두어가 일치하고 phone_book[i]!=idx일 때 false를 return함으로써 if문 하나를 줄였다.
  */
