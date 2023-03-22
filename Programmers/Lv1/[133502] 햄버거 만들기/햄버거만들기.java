537f6ac531b76ba968e33aa83694085a5585b996
eachkwon@naver.com <eachkwon@naver.com>
2023-03-23 02:34:59 +0900

    Revise: 03.23 / [햄버거 만들기]

--- Programmers/Lv1/[133502] 햄버거 만들기/햄버거만들기.java
+++ Programmers/Lv1/[133502] 햄버거 만들기/햄버거만들기.java
@@ -1,20 +1,26 @@
 package Programmers.Lv1;
 
+import java.util.Stack;
+
 class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
 
-        StringBuffer sb = new StringBuffer();
+        Stack<Integer> stack = new Stack<>();
+
         for(int n: ingredient) {
-            sb.append(String.valueOf(n));
-        }
+            stack.push(n);
+            int size = stack.size();
 
-        String str = sb.toString();
-        while(str.contains("1231")) {
-            int idx = str.indexOf("1231");
-
-            str = str.substring(0,idx) + str.substring(idx+4);
-            answer++;
+            if(size>=4) {
+                if(  stack.get(size-4)==1
+                        && stack.get(size-3)==2
+                        && stack.get(size-2)==3
+                        && stack.get(size-1)==1) {
+                    answer++;
+                    stack.pop(); stack.pop(); stack.pop(); stack.pop();
+                }
+            }
         }
 
         return answer;
@@ -22,7 +28,10 @@ class Solution {
 }
 
 /*
-5760.25ms
-7201.77ms
-5837.08ms
+5760.25ms → 48.69ms
+7201.77ms → 44.42ms
+5837.08ms → 40.97ms
+
+1. String으로 변환해 1231(햄버거 순서)이 없어질 때 까지 반복하는 코드로 구현하였다. 테스트 5, 12에서 시간초과가 발생했다.
+2. ingredient의 숫자를 Stack에 하나씩 담아 Stack 사이즈가 4이상이고 마지막 네자리 숫자가 1231일 때 4개를 꺼내는 코드를 구현하였다.
  */
