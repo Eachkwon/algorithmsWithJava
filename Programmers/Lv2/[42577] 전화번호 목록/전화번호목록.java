34b436f3d7bbc74955a0d5c1d08d68c235ba7356
eachkwon@naver.com <eachkwon@naver.com>
2023-03-29 21:37:04 +0900

    Revise: 03.29 / [전화번호 목록]

--- Programmers/Lv2/[42577] 전화번호 목록/전화번호목록.java
+++ Programmers/Lv2/[42577] 전화번호 목록/전화번호목록.java
@@ -1,7 +1,5 @@
 package Programmers.Lv2;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Arrays;
 
 class Solution {
@@ -9,16 +7,8 @@ class Solution {
         boolean answer = true;
         Arrays.sort(phone_book);
 
-        HashMap<String,Integer> map = new HashMap<>();
-        for(int i=0; i<phone_book.length; i++) {
-            map.put(phone_book[i],i);
-        }
-
-        for(Map.Entry<String,Integer> entry: map.entrySet()) {
-            for(int i=entry.getValue()+1; i<phone_book.length; i++) {
-                if(phone_book[i].startsWith(entry.getKey()))
-                    return false;
-            }
+        for(int i=0; i<phone_book.length-1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])) return false;
         }
 
         return answer;
@@ -26,9 +16,9 @@ class Solution {
 }
 
 /*
-97.71ms → 61.70ms → 48.38ms
-10.54ms → 18.42ms → 7.62ms
-115.57ms → 92.40ms → 47.53ms
+97.71ms → 61.70ms → 48.38ms → 3.91ms
+10.54ms → 18.42ms → 7.62ms → 4.63ms
+115.57ms → 92.40ms → 47.53ms → 4.59ms
 
 1. 같은 접두어를 가질 경우 value 값에 1씩 더해주고, for문을 돌린 뒤 value가 2 이상일 때 false를 return하였다.
 2. value에 idx를 넣고, 접두어가 일치하고 phone_book[i]!=idx일 때 false를 return함으로써 if문 하나를 줄였다.
