e565bf0f789a00d83642453d48a4b4c7bf6ffd99
eachkwon@naver.com <eachkwon@naver.com>
2023-04-08 22:53:34 +0900

    Upload: 04.08 / [타겟 넘버]

---
+++ Programmers/Lv2/[43165] 타겟 넘버/타겟넘버.java
@@ -0,0 +1,27 @@
+package Programmers.Lv2;
+
+class Solution {
+    int answer;
+    int[] numbers;
+    int target;
+
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        this.numbers = numbers;
+        this.target = target;
+
+        dfs(0,0);
+
+        return answer;
+    }
+
+    void dfs(int index, int sum) {
+        if(index==numbers.length) {
+            if(sum==target) answer++;
+            return;
+        }
+
+        dfs(index+1, sum + numbers[index]);
+        dfs(index+1, sum - numbers[index]);
+    }
+}
